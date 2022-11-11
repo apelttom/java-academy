ef5e937932654f719e80e6cb54dc7811c2d2096f
Tom Apeltauer <apelttom@users.noreply.github.com>
2022-11-11 10:32:30 +0100

    New version

--- TimesheetApp.java
+++ TimesheetApp.java
@@ -1,14 +1,26 @@
 import java.io.FileWriter;
 
 public class TimesheetApp {
-    public static void main(String args[]) {
-	try{
-		FileWriter writer = new FileWriter("timeLog.txt", true);
-		writer.append("Working 8 hours CEZ project.\n");
-		writer.close();
-	} catch (Exception e) {
-		e.printStackTrace();
-	}
-    }
+    public static void main(String inputArguments[]) {
+        String fileName = "timesheet.csv";
+        System.out.println("==================================");
+        System.out.println("Welcome to KPMG Timesheet Program!");
+        System.out.println("==================================");
 
+
+        System.out.println("Input argument number 1: " + inputArguments[0]);
+        System.out.println("Input argument number 2: " + inputArguments[1]);
+        System.out.println("Input argument number 3: " + inputArguments[2]);
+
+
+
+        System.out.println("We will now log 8 hours of work today on CEZ project into file: " + fileName);
+        try{
+                FileWriter writer = new FileWriter(fileName, true);
+                writer.append("Working 8 hours CEZ project.\n");
+                writer.close();
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
+    }
 }
