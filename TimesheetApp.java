fea0d93007e53f17cd9027565806be0f01513556
Tom Apeltauer <apelttom@users.noreply.github.com>
2022-11-28 10:23:23 +0100

    Working with arrays

--- TimesheetApp.java
+++ TimesheetApp.java
@@ -6,21 +6,25 @@ public class TimesheetApp {
         System.out.println("==================================");
         System.out.println("Welcome to KPMG Timesheet Program!");
         System.out.println("==================================");
+        
+        int[] arrayOfNumbers = new int[999999999];
+        arrayOfNumbers[0] = 5;
+        arrayOfNumbers[1] = 6;
 
 
-        System.out.println("Input argument number 1: " + inputArguments[0]);
-        System.out.println("Input argument number 2: " + inputArguments[1]);
-        System.out.println("Input argument number 3: " + inputArguments[2]);
+        //System.out.println("Input argument number 1: " + inputArguments[0]);
+        //System.out.println("Input argument number 2: " + inputArguments[1]);
+        //System.out.println("Input argument number 3: " + inputArguments[2]);
 
 
 
-        System.out.println("We will now log 8 hours of work today on CEZ project into file: " + fileName);
-        try{
-                FileWriter writer = new FileWriter(fileName, true);
-                writer.append("Working 8 hours CEZ project.\n");
-                writer.close();
-        } catch (Exception e) {
-                e.printStackTrace();
-        }
+        //System.out.println("We will now log 8 hours of work today on CEZ project into file: " + fileName);
+        //try{
+        //        FileWriter writer = new FileWriter(fileName, true);
+        //        writer.append("Working 8 hours CEZ project.\n");
+        //        writer.close();
+        //} catch (Exception e) {
+        //        e.printStackTrace();
+        //}
     }
 }
